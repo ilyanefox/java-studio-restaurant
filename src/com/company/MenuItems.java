9c99739cfe46b63d621a4bdc6fddfc7e1aee57a6
Ilyane <ilyaneroey@gmail.com>
2021-09-15 19:44:05 -0500

    first commit

---
+++ src/com/company/MenuItems.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class MenuItems {
+    private String description;
+    private String category;
+    private double price;
+    private boolean isNew;
+
+    public MenuItems(double price, String category, String description) {
+        this.price = price;
+        this.category = category;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+}
+
